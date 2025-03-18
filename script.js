fe89db7a9f68a89ef66fca4cd9f8f7ed406a8bdd
Mauro FJ <zona.marques@gmail.com>
2025-03-18 19:19:11 -0300

    feat: making slide of section cloths

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+import Slide from './slide.js'
+
+const slide = new Slide('.slide', '.slide-wrapper');
+slide.init();
+console.log(slide)
+
 const menuProduto = document.querySelector('.produto');
 
 function showMenu() {
