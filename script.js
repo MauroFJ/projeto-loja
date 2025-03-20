2c2b75c08395314a57c57d7c62242f62b6349984
Mauro FJ <zona.marques@gmail.com>
2025-03-19 23:14:55 -0300

    fix: slide buttons of one section

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-import Slide from './slide.js'
+// import Slide from './slide.js'
 
-const slide = new Slide('.slide', '.slide-wrapper');
-slide.init();
-console.log(slide)
+// const slide = new Slide('.slide', '.slide-wrapper');
+// slide.init();
+// console.log(slide)
 
-slide.activePrevSlide()
+// slide.activePrevSlide()
 
 const menuProduto = document.querySelector('.produto');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const cloths = document.querySelector('.clothes')
+
 
 function showMenu() {
   const openMenu = document.querySelector('.produtos')
@@ -14,4 +18,20 @@ function showMenu() {
   openMenu.classList.toggle('showMenu');
 }
 
+function moveRight() {
+  prev.style.transform = 'translate3d(300px, 0, 0)'
+  this.style.transform = 'translate3d(300px, 0, 0)'
+  cloths.style.transform = 'translate3d(-300px, 0, 0)'
+
+}
+
+function moveLeft() {
+  next.style.transform = 'translate3d(0, 0, 0)'
+  this.style.transform = 'translate3d(0, 0, 0)'
+  cloths.style.transform = 'translate3d(0, 0, 0)'
+}
+
+
 menuProduto.addEventListener('click', showMenu);
+next.addEventListener('click', moveRight);
+prev.addEventListener('click', moveLeft);
